260a968afc959675b92d2aa574f09f0aac125ce8
LeoLiao2 <poiuy301@hotmail.com>
2016-02-03 19:44:38 +0800

    update test4 git update6

--- GitTestProj1/src/test4.java
+++ GitTestProj1/src/test4.java
@@ -3,8 +3,10 @@ public class test4 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("git update5");
-		System.out.println("git update5");
+		System.out.println("git update6");
+		System.out.println("git update6");
+		System.out.println("git update6");
+		System.out.println("git update6");
 
 	}
 
