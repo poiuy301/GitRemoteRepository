267ba94e30ab1c0d7d340a08c0bcad131a4aacbe
LeoLiao <poiuy301@hotmail.com>
2016-02-03 14:29:25 +0800

    ccc

--- GitTestProj1/src/test1.java
+++ GitTestProj1/src/test1.java
@@ -3,7 +3,7 @@ public class test1 {
 
 	public static void main(String[] args) {
 		System.out.println("commit2");
-		System.out.println("update1 in master");
+		System.out.println("update1 in bugfix");
 
 	}
 
